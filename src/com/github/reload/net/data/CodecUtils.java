d3ee409e68f14660e4024526ae317811f2e37be7
zdenial <zdenial@gmx.com>
2014-02-02 23:53:54 +0100

    Use field class

--- src/com/github/reload/net/data/CodecUtils.java
+++ src/com/github/reload/net/data/CodecUtils.java
@@ -9,6 +9,32 @@ import io.netty.buffer.ByteBuf;
  */
 public class CodecUtils {
 
+	/**
+	 * The amount of bytes needed to represent an unsigned integer up to
+	 * 2<sup>8</sup>-1
+	 */
+	public static final int U_INT8 = 1;
+	/**
+	 * The amount of bytes needed to represent an unsigned integer up to
+	 * 2<sup>16</sup>-1
+	 */
+	public static final int U_INT16 = 2;
+	/**
+	 * The amount of bytes needed to represent an unsigned integer up to
+	 * 2<sup>24</sup>-1
+	 */
+	public static final int U_INT24 = 3;
+	/**
+	 * The amount of bytes needed to represent an unsigned integer up to
+	 * 2<sup>32</sup>-1
+	 */
+	public static final int U_INT32 = 4;
+	/**
+	 * The amount of bytes needed to represent an unsigned integer up to
+	 * 2<sup>64</sup>-1
+	 */
+	public static final int U_INT64 = 8;
+
 	private CodecUtils() {
 	}
 
@@ -22,70 +48,10 @@ public class CodecUtils {
 	 * @param buf
 	 *            the buffer
 	 * @param maxDataLength
-	 *            the data maximum size in bytes
+	 *            the data maximum bytes size in power of two
 	 */
-	public static int allocateField(ByteBuf buf, int maxDataLength) {
-		int fieldPos = buf.writerIndex();
-		buf.writerIndex(fieldPos + maxDataLength);
-		return fieldPos;
-	}
-
-	/**
-	 * Set data length to the length subfield at the given field position.
-	 * The data length is calculated starting from the end of the length
-	 * subfield up to the current buffer write index
-	 * 
-	 * @param buf
-	 *            the buffer
-	 * @param fieldPos
-	 *            the start position of the field
-	 * @param maxDataLength
-	 *            the data maximum size in bytes
-	 * 
-	 * @return
-	 *         the length of data subfield in bytes
-	 * 
-	 * @throws IndexOutOfBoundsException
-	 *             if the written bytes after the length subfield exceeds the
-	 *             data maximum length
-	 */
-	public static int setDataLength(ByteBuf buf, int fieldPos, int maxDataLength) {
-		// current position - (start of data subfield)
-		int writtenDataLength = buf.writerIndex() - (fieldPos + maxDataLength);
-
-		if (writtenDataLength > maxDataLength)
-			throw new IndexOutOfBoundsException("Trying to write " + writtenDataLength + " bytes where the maximum field length is " + maxDataLength + " bytes");
-
-		// Set actual written data subfield length into the length subfield
-		buf.writerIndex(fieldPos);
-		encodeLength(maxDataLength, writtenDataLength, buf);
-
-		// Reset 1 byte after original position (after the field)
-		buf.writerIndex(buf.writerIndex() + writtenDataLength + 1);
-
-		return writtenDataLength;
-	}
-
-	/**
-	 * Encode length subfield
-	 */
-	private static void encodeLength(int maxDataLength, int dataLength, ByteBuf buf) {
-		int neededBytes = getNeededBytes(maxDataLength);
-		for (int i = 0; i < neededBytes; i++) {
-			int offset = (neededBytes - 1 - i);
-			buf.writeByte(dataLength >>> offset);
-		}
-	}
-
-	/**
-	 * The mininum amount of bytes needed to represent the given value as an
-	 * unsigned integer
-	 */
-	private static int getNeededBytes(int value) {
-		int s = 1;
-		while (s < 8 && value >= (1L << (s * 8)))
-			s++;
-		return s;
+	public static Field allocateField(ByteBuf buf, int maxDataLength) {
+		return new Field(buf, maxDataLength);
 	}
 
 	/**
@@ -95,16 +61,60 @@ public class CodecUtils {
 	 * @param buf
 	 *            the buffer
 	 * @param maxDataLength
-	 *            the data maximum size in bytes
+	 *            the data maximum bytes size in power of two
 	 */
 	public static int readDataLength(ByteBuf buf, int maxDataLength) {
-		int neededBytes = getNeededBytes(maxDataLength);
 		int decoded = 0;
-		int baseOffset = (neededBytes - 1) * 8;
-		for (int i = 0; i < neededBytes; i++) {
+		int baseOffset = (maxDataLength - 1) * 8;
+		for (int i = 0; i < maxDataLength; i++) {
 			int offset = baseOffset - (i * 8);
 			decoded += (buf.readByte() & 0xff) << offset;
 		}
 		return decoded;
+	}
+
+	public static class Field {
+
+		private final ByteBuf buf;
+		private final int fieldPos;
+		private final int maxDataLength;
+
+		public Field(ByteBuf buf, int maxDataLength) {
+			this.buf = buf;
+			fieldPos = buf.writerIndex();
+			this.maxDataLength = maxDataLength;
+			buf.writerIndex(fieldPos + maxDataLength);
+		}
+
+		/**
+		 * Set data length of the field, its calculated starting from the end of
+		 * the length subfield up to the current buffer write index
+		 * 
+		 * @return
+		 *         the length of data subfield in bytes
+		 */
+		public int updateDataLength() {
+			// current position - (start of data subfield)
+			int writtenDataLength = buf.writerIndex() - (fieldPos + maxDataLength);
+			// Set actual written data subfield length into the length subfield
+			buf.writerIndex(fieldPos);
+			encodeLength(maxDataLength, writtenDataLength, buf);
+
+			// Reset 1 byte after original position (after the field)
+			buf.writerIndex(buf.writerIndex() + writtenDataLength + 1);
+
+			return writtenDataLength;
+		}
+
+		/**
+		 * Encode length subfield
+		 */
+		private static void encodeLength(int maxDataLength, int dataLength, ByteBuf buf) {
+			for (int i = 0; i < maxDataLength; i++) {
+				int offset = (maxDataLength - 1 - i);
+				buf.writeByte(dataLength >>> offset);
+			}
+		}
+
 	}
 }
