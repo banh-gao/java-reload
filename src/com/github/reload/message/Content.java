c5d012f2a81b8eb1753748eadab4ba833ba874af
zdenial <zdenial@gmx.com>
2014-02-03 22:11:27 +0100

    Add encodable capability

--- src/com/github/reload/message/Content.java
+++ src/com/github/reload/message/Content.java
@@ -1,8 +1,22 @@
 package com.github.reload.message;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.EncoderException;
+import com.github.reload.net.data.Encodable;
+
 /**
  * RELOAD message content
  */
-public class Content {
+public class Content implements Encodable {
+
+	@Override
+	public void encode(ByteBuf buf) throws EncoderException {
+		// TODO Auto-generated method stub
+	}
+
+	public static Content decode(ByteBuf buf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/com/github/reload/message/Header.java
+++ src/com/github/reload/message/Header.java
@@ -1,8 +1,33 @@
 package com.github.reload.message;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.EncoderException;
+import com.github.reload.net.data.Encodable;
+
 /**
  * RELOAD message header
  */
-public class Header {
+public class Header implements Encodable {
+
+	/**
+	 * Size in bytes of the first part of the header from the beginning to the
+	 * message length field
+	 */
+	public static int HDR_LEADING_LEN = 128;
+
+	public int getPayloadLength() {
+		// TODO: return payload length
+		return 0;
+	}
+
+	public static Header decode(ByteBuf buf) {
+		// TODO : decode header
+		return null;
+	}
+
+	@Override
+	public void encode(ByteBuf buf) throws EncoderException {
+		// TODO : encode header
+	}
 
 }
