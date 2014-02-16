0ac2e4cbe55fbc44ef0fd8b25f596076a6c26f71
zdenial <zdenial@gmx.com>
2014-02-16 02:39:34 +0100

    Mapping Message Code -> Message class

--- src/com/github/reload/message/ContentType.java
+++ src/com/github/reload/message/ContentType.java
@@ -1,18 +1,7 @@
 package com.github.reload.message;
 
 import java.util.EnumSet;
-import com.github.reload.message.content.AppAttachReqAns;
-import com.github.reload.message.content.AttachMessage;
-import com.github.reload.message.content.ConfigUpdateAnswer;
-import com.github.reload.message.content.ConfigUpdateRequest;
-import com.github.reload.message.content.JoinAnswer;
-import com.github.reload.message.content.JoinRequest;
-import com.github.reload.message.content.LeaveRequest;
-import com.github.reload.message.content.PingAnswer;
-import com.github.reload.message.content.PingRequest;
-import com.github.reload.message.content.ProbeAnswer;
-import com.github.reload.message.content.ProbeRequest;
-import com.github.reload.message.content.RouteQueryRequest;
+import com.github.reload.message.content.*;
 import com.github.reload.message.errors.Error;
 import com.github.reload.storage.FetchAnswer;
 import com.github.reload.storage.FetchRequest;
@@ -31,220 +20,39 @@ import com.github.reload.storage.StoreRequest;
  * 
  */
 public enum ContentType {
-	PROBE_REQ((short) 1) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return ProbeRequest.class;
-		}
-	},
-	PROBE_ANS((short) 2) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return ProbeAnswer.class;
-		}
-	},
-	ATTACH_REQ((short) 3) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return AttachMessage.class;
-		}
-	},
-	ATTACH_ANS((short) 4) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return AttachMessage.class;
-		}
-	},
-	STORE_REQ((short) 7) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return StoreRequest.class;
-		}
-	},
-	STORE_ANS((short) 8) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return StoreAnswer.class;
-		}
-	},
-	FETCH_REQ((short) 9) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return FetchRequest.class;
-		}
-	},
-	FETCH_ANS((short) 10) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return FetchAnswer.class;
-		}
-	},
-	FIND_REQ((short) 13) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return FindRequest.class;
-		}
-	},
-	FIND_ANS((short) 14) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return FindAnswer.class;
-		}
-	},
-	JOIN_REQ((short) 15) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return JoinRequest.class;
-		}
-	},
-	JOIN_ANS((short) 16) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return JoinAnswer.class;
-		}
-	},
-	LEAVE_REQ((short) 17) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return LeaveRequest.class;
-		}
-	},
-	UPDATE_REQ((short) 19) {
-
-		@Override
-		public MessageContent parseContent(Context context, UnsignedByteBuffer buf) {
-			MessageContent content = context.getTopologyPlugin().parseUpdateRequest(buf);
-			if (content.getType() != ContentType.UPDATE_REQ)
-				throw new RuntimeException("The topology content must be of type " + ContentType.UPDATE_REQ);
-			return content;
-		}
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	},
-	UPDATE_ANS((short) 20) {
-
-		@Override
-		public MessageContent parseContent(Context context, UnsignedByteBuffer buf) {
-			MessageContent content = context.getTopologyPlugin().parseUpdateAnswer(buf);
-			if (content.getType() != ContentType.UPDATE_ANS)
-				throw new RuntimeException("The topology content must be of type " + ContentType.UPDATE_ANS);
-			return content;
-		}
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	},
-	ROUTE_QUERY_REQ((short) 21) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return RouteQueryRequest.class;
-		}
-	},
-	ROUTE_QUERY_ANS((short) 22) {
-
-		@Override
-		public MessageContent parseContent(Context context, UnsignedByteBuffer buf) {
-			MessageContent content = context.getTopologyPlugin().parseRouteQueryAnswer(buf);
-			if (content.getType() != ContentType.ROUTE_QUERY_ANS)
-				throw new RuntimeException("The topology content must be of type " + ContentType.ROUTE_QUERY_ANS);
-			return content;
-		}
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-	},
-	PING_REQ((short) 23) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return PingRequest.class;
-		}
-	},
-	PING_ANS((short) 24) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return PingAnswer.class;
-		}
-	},
-	STAT_REQ((short) 25) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return StatRequest.class;
-		}
-	},
-	STAT_ANS((short) 26) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return StatAnswer.class;
-		}
-	},
-	APPATTACH_REQ((short) 29) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return AppAttachReqAns.class;
-		}
-	},
-	APPATTACH_ANS((short) 30) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return AppAttachReqAns.class;
-		}
-	},
-	CONFIG_UPDATE_REQ((short) 33) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return ConfigUpdateRequest.class;
-		}
-	},
-	CONFIG_UPDATE_ANS((short) 34) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return ConfigUpdateAnswer.class;
-		}
-	},
-	ERROR((short) 0xffff) {
-
-		@Override
-		public Class<? extends Content> getContentClass() {
-			return Error.class;
-		}
-	};
+	PROBE_REQ((short) 1,ProbeRequest.class),
+	PROBE_ANS((short) 2,ProbeAnswer.class),
+	ATTACH_REQ((short) 3,AttachMessage.class),
+	ATTACH_ANS((short) 4,AttachMessage.class),
+	STORE_REQ((short) 7,StoreRequest.class),
+	STORE_ANS((short) 8,StoreAnswer.class),
+	FETCH_REQ((short) 9,FetchRequest.class),
+	FETCH_ANS((short) 10,FetchAnswer.class),
+	FIND_REQ((short) 13,FindRequest.class),
+	FIND_ANS((short) 14,FindAnswer.class),
+	JOIN_REQ((short) 15,JoinRequest.class),
+	JOIN_ANS((short) 16,JoinAnswer.class),
+	LEAVE_REQ((short) 17,LeaveRequest.class),
+	UPDATE_REQ((short) 19,UpdateRequest.class),
+	UPDATE_ANS((short) 20, UpdateAnswer.class),
+	ROUTE_QUERY_REQ((short) 21, RouteQueryRequest.class),
+	ROUTE_QUERY_ANS((short) 22), RouteQueryAnswer.class),
+	PING_REQ((short) 23, PingRequest.class),
+	PING_ANS((short) 24, PingAnswer.class),
+	STAT_REQ((short) 25, StatRequest.class),
+	STAT_ANS((short) 26, StatAnswer.class),
+	APPATTACH_REQ((short) 29, AppAttachReqAns.class),
+	APPATTACH_ANS((short) 30, AppAttachReqAns.class),
+	CONFIG_UPDATE_REQ((short) 33, ConfigUpdateRequest.class),
+	CONFIG_UPDATE_ANS((short) 34, ConfigUpdateAnswer.class),
+	ERROR((short) 0xffff, Error.class);
 
 	private final short code;
+	private final Class<? extends Content> contentClass;
 
-	private ContentType(short code) {
+	private ContentType(short code, Class<? extends Content> contentClass) {
 		this.code = code;
+		this.contentClass = contentClass;
 	}
 
 	public static ContentType valueOf(short code) {
@@ -259,9 +67,11 @@ public enum ContentType {
 	}
 
 	/**
-	 * Parses the message content for this message type
+	 * @return the content class that implements this message type
 	 */
-	public abstract Class<? extends Content> getContentClass();
+	public Class<? extends Content> getContentClass() {
+		return contentClass;
+	}
 
 	public boolean isRequest() {
 		if (this == ContentType.ERROR)
